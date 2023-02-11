f33ca0e332a28c7cd4f90a1aeb9580873627c972
Christian <111076892+i-christian@users.noreply.github.com>
2023-02-11 19:58:31 +0200

    created new classes

--- console.py
+++ console.py
@@ -10,6 +10,11 @@ from models.user import User
 import models
 from models import storage
 from models.base_model import BaseModel
+from models.state import State
+from models.city import City
+from models.place import Place
+from models.amenity import Amenity
+from models.review import Review
 
 
 def parse(arg):
