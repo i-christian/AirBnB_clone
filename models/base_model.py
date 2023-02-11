5e634c7c62f1bc70cb3d61665cafec1a6c632279
Christian <111076892+i-christian@users.noreply.github.com>
2023-02-11 17:12:19 +0200

    first update to base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -20,15 +20,18 @@ class BaseModel:
         self.updated_at = datetime.now()
         if kwargs:
             for key, value in kwargs.items():
-                if key == 'created_at' or key == 'updated_at':
-                    value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
+                if key != "__class__":
                     setattr(self, key, value)
+            if 'created_at' in kwargs:
+                self.created_at = datetime.strptime(kwargs['created_at'], '%Y-%m-%dT%H:%M:%S.%f')
+            if 'updated_at' in kwargs:
+                self.updated_at = datetime.strptime(kwargs['updated_at'], '%Y-%m-%dT%H:%M:%S.%f')
         else:
             models.storage.new(self)
 
     def save(self):
         """Update updated_at with the current datetime."""
-        self.updated_at = datetime.today()
+        self.updated_at = datetime.now()
         models.storage.save()
 
     def to_dict(self):
@@ -40,9 +43,10 @@ class BaseModel:
         obj_dict = self.__dict__.copy()
         obj_dict["created_at"] = self.created_at.isoformat()
         obj_dict["updated_at"] = self.updated_at.isoformat()
-        obj_dict["__class__"] = self.__class__.__name__
+        #obj_dict["__class__"] = self.__class__.__name__
+        obj_dict['__class__'] = type(self).__name__
         return obj_dict
 
     def __str__(self):
         """Returns a string representation of a BaseModel instance"""
-        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+        return "[{}] ({}) {}".format(type(self).__name__, self.id, self.__dict__)
