bdb2b244211ef967dd6f9f2f9750c97637c12d93
Christian <111076892+i-christian@users.noreply.github.com>
2023-10-13 23:47:01 +0200

    update base model

--- models/base_model.py
+++ models/base_model.py
@@ -18,6 +18,15 @@ class BaseModel:
         time_format = "%Y-%m-%dT%H:%M:%S.%f"
         self.created_at = datetime.today()
         self.updated_at = datetime.today()
+        if len(kwargs) == 0:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.today()
+        else:
+            for key, value in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    self.__dict__[key] = datetime.strptime(value, time_format)
+                else:
+                    self.__dict__[key] = value
 
     def save(self):
         """Updates the public instance attribute (updated_at) with the current
